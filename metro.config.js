dae774dd5bb778723b973a3d1c67856ab24f510e
Supermandi Tech <supermanditech@gmail.com>
2026-01-23 14:27:42 +0530

    chore(metro): ignore backend folder for Windows watcher stability

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
+
 const config = getDefaultConfig(__dirname);
 
+// Ignore backend (pnpm creates temp folders that break Metro watcher on Windows)
 config.watchFolders = [__dirname];
 config.resolver.blockList = [
   /.*\/backend\/.*/,
